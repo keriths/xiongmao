5d1e57aff804a0d1c903aec7ad367a8df1b7072f
shuai.fan <shuai.fan@dianping.com>
2017-10-22 09:27:07 +0800

    add test code

---
+++ src/main/java/com.xm.web/TestAction.java
@@ -0,0 +1,7 @@
+package com.xm.web;
+
+/**
+ * Created by fanshuai on 17/10/22.
+ */
+public class TestAction {
+}
---
+++ src/main/java/com/xm/service/TestService.java
@@ -0,0 +1,7 @@
+package com.xm.service;
+
+/**
+ * Created by fanshuai on 17/10/22.
+ */
+public class TestService {
+}
