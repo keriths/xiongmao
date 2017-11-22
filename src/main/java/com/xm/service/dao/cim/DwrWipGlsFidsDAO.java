f5985691cf5912d101484a5ae62b9835e07a54ad
luokaiming <1668940681@qq.com>
2017-11-22 13:11:00 +0800

    '在制品接口'

--- src/main/java/com/xm/service/dao/cim/DwrWipGlsFidsDAO.java
+++ src/main/java/com/xm/service/dao/cim/DwrWipGlsFidsDAO.java
@@ -1,7 +1,12 @@
 package com.xm.service.dao.cim;
 
+import com.xm.platform.annotations.ApiServiceDoc;
 import com.xm.service.apiimpl.pc.cim.goodinprocess.dto.GoodInProcessFtRetDTO;
+import com.xm.service.apiimpl.pc.cim.goodinprocess.dto.GoodInProcessWipDataDTO;
+import com.xm.service.apiimpl.pc.cim.goodinprocess.dto.GoodInProcessWipRetDTO;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
@@ -9,6 +14,7 @@ import java.util.List;
 /**
  * Created by Administrator on 2017/11/22.
  */
+@Repository("dwrWipGlsFidsDAO")
 public interface DwrWipGlsFidsDAO {
 
     List<GoodInProcessFtRetDTO.GoodInProcessFtDate> queryGoodInProcessFtDate(
@@ -18,4 +24,7 @@ public interface DwrWipGlsFidsDAO {
             @Param("beginDate")Date beginDate,
             @Param("endDate")Date endDate
     );
+
+    List<GoodInProcessWipDataDTO> queryGoodInProcessWip(@Param("beginDate")Date beginDate,
+                                                        @Param("endDate")Date endDate);
 }
