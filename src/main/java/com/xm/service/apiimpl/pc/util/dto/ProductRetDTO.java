b6031c697858d49436d3af9051e9f4091dd7c057
Wang0325 <1119710399@qq.com>
2017-11-22 18:18:32 +0800

    '333'

--- src/main/java/com/xm/service/apiimpl/pc/util/dto/ProductRetDTO.java
+++ src/main/java/com/xm/service/apiimpl/pc/util/dto/ProductRetDTO.java
@@ -1,14 +1,20 @@
 package com.xm.service.apiimpl.pc.util.dto;
 
+import com.xm.platform.annotations.ApiResultFieldDesc;
+
 import java.util.List;
 
 /**
- * Created by Administrator on 2017/11/22 0022.
+ * Created by luokaiming on 2017/11/22 0022.
  */
 public class ProductRetDTO {
-    List<productRetDataDTO> dataDTOList;
+    @ApiResultFieldDesc(desc = "产品数据列表")
+    private List<productRetDataDTO> dataDTOList;
+
     public static class  productRetDataDTO {
+        @ApiResultFieldDesc(desc = "产品ID")
         private String productId;
+        @ApiResultFieldDesc(desc = "产品名称")
         private String productName;
 
         public String getProductId() {
