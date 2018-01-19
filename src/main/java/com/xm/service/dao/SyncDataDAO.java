2cdde6790573909e29875287fad6b5819645cafd
shuai.fan <shuai.fan@dianping.com>
2018-01-19 08:48:07 +0800

    a

--- src/main/java/com/xm/service/dao/SyncDataDAO.java
+++ src/main/java/com/xm/service/dao/SyncDataDAO.java
@@ -11,4 +11,12 @@ public interface SyncDataDAO {
     List<Map> queryFactoryDataList(String tableName,
                                    List<String> queryAndSaveColumns,
                                    List<String> orderByCondition);
+
+    Map loadData(String tableName,List<String> primeKeyColumns,Map data);
+
+    int updateData(String tableName,
+                   List<String> queryAndSaveColumns,List<String> primeKeyColumns,Map data);
+
+    int saveData(String tableName,
+                 List<String> queryAndSaveColumns,Map data);
 }
