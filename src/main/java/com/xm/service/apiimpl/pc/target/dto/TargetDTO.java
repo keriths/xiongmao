2f425caa05688133accc59e417078937f11fdee6
shuai.fan <shuai.fan@dianping.com>
2018-02-14 13:27:21 +0800

    target

---
+++ src/main/java/com/xm/service/apiimpl/pc/target/dto/TargetDTO.java
@@ -0,0 +1,40 @@
+package com.xm.service.apiimpl.pc.target.dto;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by wangshuna on 2018/2/9.
+ */
+public class TargetDTO {
+
+    private String factory;
+    private String productId;
+    private BigDecimal targetInline;
+
+
+
+
+    public String getFactory() {
+        return factory;
+    }
+
+    public void setFactory(String factory) {
+        this.factory = factory;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getTargetInline() {
+        return targetInline;
+    }
+
+    public void setTargetInline(BigDecimal targetInline) {
+        this.targetInline = targetInline;
+    }
+}
