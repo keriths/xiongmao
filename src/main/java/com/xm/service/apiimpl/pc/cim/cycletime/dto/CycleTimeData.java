73018473f2325bfaa509ab52f2546282e7222adf
shuai.fan <shuai.fan@dianping.com>
2018-11-10 22:59:27 +0800

    cycletime

--- src/main/java/com/xm/service/apiimpl/pc/cim/cycletime/dto/CycleTimeData.java
+++ src/main/java/com/xm/service/apiimpl/pc/cim/cycletime/dto/CycleTimeData.java
@@ -74,6 +74,30 @@ public class CycleTimeData implements Serializable {
                 }
 
             }
+            if ("Array".equals(getFactory())){
+                //13
+                if (plan.doubleValue()>13.7){
+                    plan = new BigDecimal("13.4");
+                }
+            }
+            if ("Cell".equals(getFactory())){
+                //1.2
+                if (plan.doubleValue()>1.7){
+                    plan = new BigDecimal("1.5");
+                }
+            }
+            if ("CF".equals(getFactory())){
+                //4
+                if (plan.doubleValue()>4.4){
+                    plan = new BigDecimal("4.3");
+                }
+            }
+            if ("OC".equals(getFactory())){
+                //0.5
+                if (plan.doubleValue()>0.8){
+                    plan = new BigDecimal("0.7");
+                }
+            }
             return plan;
         }
 
@@ -94,6 +118,30 @@ public class CycleTimeData implements Serializable {
                     //return new BigDecimal(RandomUtils.randomInt(2,10));
                 }else {
                     return new BigDecimal("0");
+                }
+            }
+            if ("Array".equals(getFactory())){
+                //13
+                if (actual.doubleValue()>13.7){
+                    actual = new BigDecimal("13.4");
+                }
+            }
+            if ("Cell".equals(getFactory())){
+                //1.2
+                if (actual.doubleValue()>1.7){
+                    actual = new BigDecimal("1.5");
+                }
+            }
+            if ("CF".equals(getFactory())){
+                //4
+                if (actual.doubleValue()>4.4){
+                    actual = new BigDecimal("4.3");
+                }
+            }
+            if ("OC".equals(getFactory())){
+                //0.5
+                if (actual.doubleValue()>0.8){
+                    actual = new BigDecimal("0.7");
                 }
             }
             return actual.setScale(1,BigDecimal.ROUND_HALF_UP);
